4cf7907c4100da0e7aec1032435d8f3c2d2b7498
Evan Gattis <evga8663@colorado.edu>
2022-11-06 17:13:38 -0700

    Map is now interactive

--- cuspots/src/index.js
+++ cuspots/src/index.js
@@ -77,19 +77,35 @@ app.get('/logout', (req, res) =>{
     message: "logged out successfully",
   });
 });
+
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+
 app.get('/map', (req, res) =>{
   if (!req.session.user) {
     // Default to register page.
     return res.redirect('/register');
   }
-  const daMap = "https://maps.googleapis.com/maps/api/staticmap?center=40.00790356892552,-105.26975530161658&zoom=16&size=2000x400&maptype=hybrid&key=" + String(req.session.user.api_key);
   const all_spots = `SELECT * FROM spots ORDER BY spots.spot_id ASC;`;
   console.log("here");
   db.any(all_spots)
     .then((spots) => {
       res.render("pages/map", {
         spots,
-        map: daMap,
+        key: req.session.user.api_key,
         session: req.session.user,
       });
     })
